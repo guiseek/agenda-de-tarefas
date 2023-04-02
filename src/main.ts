2e9f3028504639a52b0a32b1e980e2a65f336bb3
Gui Seek <guiseek@gmail.com>
2023-04-02 18:09:51 -0300

    comentários

--- src/main.ts
+++ src/main.ts
@@ -7,34 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form')
   const inputDate = form.querySelector<HTMLInputElement>('#date')
   const dialog = document.querySelector('dialog')
-  dialog.showModal()
 
+  /* Mostra dialog de instruções */
+  dialog.showModal()
+  
+  /* Evento de submissão */
   form.onsubmit = (ev) => {
     ev.preventDefault()
 
+    /*  Dados do formulário */
     const data = new FormData(form)
 
-    /**
-     * Envia para armazenar tarefa
-     */
+    /* Envia para armazenar */
     setTasks(formatTask(data))
   }
 
+  /* Calendário */
+  const calendar = document.querySelector('type-calendar')
+
+  /* Evento disparado quando um dia é selecionado */
+  calendar.addEventListener('selected', ({detail}: CustomEvent<DayDataSet>) => {
+    const date = formatFromCalendar(detail)
+    /* Altera a data do input date,
+    com a data do dia selecionado */
+    inputDate.value = date
+
+    /* Busca lista de tarefas */
+    const tasks = getTasks(date)
+
+    /* Mostra lista no console */
+    console.log(tasks)
+
+    /**
+     * E aqui você pode trabalhar criando os
+     * itens que serão adicionados 👇 no HTML
+     */
+    tasks.forEach((task) => {})
+  })
+
+  /* Quando o campo de data é alterado,
+    fazemos a alteração no calendário */
   inputDate.onchange = () => {
     const {year, month, day} = explodeDateFromForm(inputDate.value)
     calendar.updateDate(+year, +month, +day)
   }
 
-  /** Calendar */
-  const calendar = document.querySelector('type-calendar')
-  calendar.addEventListener('selected', ({detail}: CustomEvent<DayDataSet>) => {
-    const date = formatFromCalendar(detail)
-    /**
-     * Verifica se tem tarefas pra este dia
-     * e retorna uma lista, mesmo que vazia
-     */
-    inputDate.value = date
-    const tasks = getTasks(date)
-    console.log(tasks)
-  })
+  /* Aguarda 6 segundos e move o scroll
+    de instruções até a 'última seção */
+  setTimeout(() => {
+    const important = dialog.querySelector('#important')
+    important.scrollIntoView({behavior: 'smooth'})
+  }, 6000)
 })
